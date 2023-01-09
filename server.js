ab81e79825f9250d14398ebd2fe420f13655b85a
JIA SWEE LAW <jiaswee@jiangxia.com.sg>
2023-01-09 09:54:47 +0800

    commit chg port

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.post("/media/upload",function (req, res, next) {
             // ERROR occurred (here it can be occurred due
             // to uploading image of size greater than
             // 1MB or uploading different file type)
-            res.send(err)
+            res.send({status: 400, path: ""})
         }
         else {
             // SUCCESS, image successfully uploaded
